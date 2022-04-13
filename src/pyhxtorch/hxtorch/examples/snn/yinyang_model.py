d0e7d604c7aa210f138683e866c041e2cb6d6d4c
Luca Blessing <luca.blessing@stud.uni-heidelberg.de>
2022-04-13 15:24:03 +0200

    Add yinyang example
    Change-Id: I8a4f1fb44be5b1e5de27c7fd968009d411733a9f

---
+++ src/pyhxtorch/hxtorch/examples/snn/yinyang_model.py
@@ -0,0 +1,201 @@
+"""
+Model class for spiking HX torch yinyang example
+"""
+from typing import Optional, Tuple
+from functools import partial
+import torch
+
+import hxtorch
+import hxtorch.snn as snn
+from hxtorch.snn.transforms import weight_transforms
+import hxtorch.snn.functional as F
+
+
+class SNN(torch.nn.Module):
+    """
+    SNN with one hidden LIF layer and one readout LI layer
+    """
+    # pylint: disable=too-many-arguments, invalid-name
+
+    def __init__(self, n_in: int, n_hidden: int, n_out: int, mock: bool,
+                 dt: float = 1.0e-6, tau_mem: float = 8e-6,
+                 tau_syn: float = 8e-6, alpha: float = 50,
+                 trace_shift_hidden: int = 0, trace_shift_out: int = 0,
+                 weight_init_hidden: Optional[Tuple[float, float]] = None,
+                 weight_init_output: Optional[Tuple[float, float]] = None,
+                 weight_scale: float = 1., trace_scale: float = 1.,
+                 input_repetitions: int = 1,
+                 device: torch.device = torch.device("cpu")) -> None:
+        """
+        Initialize the SNN.
+
+        :param n_in: Number of input units.
+        :param n_hidden: Number of hidden units.
+        :param n_out: Number of output units.
+        :param mock: Indicating whether to train in software or on hardware.
+        :param dt: Time-binning width.
+        :param tau_mem: Membrane time constant.
+        :param tau_syn: Synaptic time constant.
+        :param trace_shift_hidden: Indicates how many indices the membrane
+            trace of hidden layer is shifted to left along time axis.
+        :param trace_shift_out: Indicates how many indices the membrane
+            trace of readout layer is shifted to left along time axis.
+        :param weight_init_hidden: Hidden layer weight initialization mean
+            and std value.
+        :param weight_init_output: Output layer weight initialization mean
+            and std value.
+        :param weight_scale: The factor with which the software weights are
+            scaled when mapped to hardware.
+        :param input_repetitions: Number of times to repeat input channels.
+        :param device: The used PyTorch device used for tensor operations in
+            software.
+        """
+        super().__init__()
+
+        # Neuron parameters
+        lif_params = F.LIFParams(
+            1. / tau_mem, 1. / tau_syn, dt=dt, alpha=alpha)
+        li_params = F.LIParams(1. / tau_mem, 1. / tau_syn, dt=dt)
+
+        # Instance to work on
+        self.instance = snn.Instance(mock=mock, dt=dt)
+
+        # Ajdust placement to use second hemisphere for output neurons
+        self.instance.neuron_placement = snn.instance.NeuronPlacement(
+            permutation=list(range(0, n_hidden)) + list(range(256, 512)))
+
+        # Repeat input
+        self.input_repetitions = input_repetitions
+        # Input projection
+        self.linear_h = snn.Synapse(
+            n_in * input_repetitions, n_hidden, instance=self.instance,
+            transform=partial(
+                weight_transforms.linear_saturating, scale=weight_scale))
+        # Initialize weights
+        if weight_init_hidden:
+            w = torch.zeros(n_hidden, n_in)
+            torch.nn.init.normal_(w, *weight_init_hidden)
+            self.linear_h.weight.data = w.repeat(1, input_repetitions)
+
+        # Hidden layer
+        self.lif_h = snn.Neuron(
+            n_hidden, instance=self.instance, func=F.lif_integration,
+            params=lif_params, trace_scale=trace_scale,
+            cadc_time_shift=trace_shift_hidden, shift_cadc_to_first=True)
+
+        # Output projection
+        self.linear_o = snn.Synapse(
+            n_hidden, n_out, instance=self.instance,
+            transform=partial(
+                weight_transforms.linear_saturating, scale=weight_scale))
+
+        # Readout layer
+        self.li_readout = snn.ReadoutNeuron(
+            n_out, instance=self.instance, func=F.li_integration,
+            params=li_params, trace_scale=trace_scale,
+            cadc_time_shift=trace_shift_out, shift_cadc_to_first=True)
+        # Initialize weights
+        if weight_init_output:
+            torch.nn.init.normal_(self.linear_o.weight, *weight_init_output)
+
+        # Device
+        self.device = device
+        self.to(device)
+
+        # placeholder for hidden spikes
+        self.s_h = None
+
+    def forward(self, spikes: torch.Tensor) -> torch.Tensor:
+        """
+        Perform a forward path.
+
+        :param spikes: NeuronHandle holding spikes as input.
+
+        :return: Returns the output of the network, i.e. membrane traces of the
+            readout neurons.
+        """
+        # Increase synapse strength by repeating each input
+        spikes = spikes.repeat(1, 1, self.input_repetitions)
+
+        # Spike input handle
+        spikes_handle = snn.NeuronHandle(spikes)
+
+        # Forward
+        c_h = self.linear_h(spikes_handle)
+        self.s_h = self.lif_h(c_h)  # Keep spikes for fire reg.
+        c_o = self.linear_o(self.s_h)
+        y_o = self.li_readout(c_o)
+
+        # Execute on hardware
+        hxtorch.snn.run(self.instance, spikes.shape[1])
+
+        return y_o.v_cadc
+
+
+class Model(torch.nn.Module):
+
+    """ Complete model with encoder, network (snn) and decoder """
+
+    def __init__(self, encoder: torch.nn.Module,
+                 network: torch.nn.Module,
+                 decoder: torch.nn.Module,
+                 readout_scale: float = 1.) -> None:
+        """
+        Initialize the model by assigning encoder, network and decoder
+
+        :param encoder: Module to encode input data
+        :param network: Network module containing layers and
+            parameters / weights
+        :param decoder: Module to decode network output
+        """
+        super().__init__()
+        self.encoder = encoder
+        self.network = network
+        self.decoder = decoder
+
+        self.readout_scale = readout_scale
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """
+        Perform forward pass through whole model, i.e.
+        data -> encoder -> network -> decoder -> output
+
+        :param inputs: tensor input data
+
+        :returns: Returns tensor output
+        """
+        spikes = self.encoder(inputs)
+        traces = self.network(spikes)
+        self.scores = self.decoder(traces).clone()
+
+        # scale outputs
+        with torch.no_grad():
+            self.scores *= self.readout_scale
+
+        return self.scores
+
+    def regularize(self, reg_readout: float, reg_bursts: float,
+                   reg_w_hidden: float, reg_w_output: float) -> torch.Tensor:
+        """
+        Get regularization terms for bursts and weights like
+        factor * (thing to be regularized) ** 2.
+
+        :param reg_bursts: prefactor of burst / hidden spike regulaization
+        :param reg_weights_hidden: prefactor of hidden weight regularization
+        :param reg_weights_output: prefactor of output weight regularization
+
+        :returns: Returns sum of regularization terms
+        """
+        reg = torch.tensor(0.)
+
+        # Reg readout
+        reg += reg_readout * torch.mean(self.scores ** 2)
+
+        # bursts (hidden spikes) regularization
+        reg += reg_bursts * torch.mean(
+            torch.sum(self.network.s_h.spikes, dim=1) ** 2.)
+        # weight regularization
+        reg += reg_w_hidden * torch.mean(self.network.linear_h.weight ** 2.)
+        reg += reg_w_output * torch.mean(self.network.linear_o.weight ** 2.)
+
+        return reg
---
+++ src/pyhxtorch/hxtorch/snn/datasets/yinyang.py
@@ -0,0 +1,165 @@
+"""
+YinYangDataset class from
+https://github.com/lkriener/yin_yang_data_set/blob/master/dataset.py
+with minor changes.
+"""
+from typing import Optional, Tuple
+import numpy as np
+
+import torch
+from torch.utils.data.dataset import Dataset
+
+
+# pylint: disable=invalid-name, unused-variable
+class YinYangDataset(Dataset):
+    """ YinYang dataset """
+
+    def __init__(self, r_small: float = 0.1, r_big: float = 0.5,
+                 size: int = 1000, seed: int = 42,
+                 transform: Optional[torch.nn.Module] = None) -> None:
+        """
+        Instantiate the YinYang dataset. This dataset provides datapoints on a
+        2-dimensional plane within a yin-yang sign. Each data point is assigned
+        to one of three classes: The eyes, the yin or the yang.
+
+        :param r_small: The radius of the eyes in the yin-yang sign.
+        :param r_big: The radius of the whole sign.
+        :param size: The size of the dataset, i.e. number of data points within
+            the sign.
+        :param seed: Random seed.
+        :param transform: An optional transformation applied to the returned
+            samples.
+        """
+        super().__init__()
+
+        # using a numpy RNG to allow compatibility to
+        # other deep learning frameworks
+        self.rng = np.random.RandomState(seed)
+
+        # radii
+        self.r_small = r_small
+        self.r_big = r_big
+
+        # transformation
+        self.transform = transform
+
+        # values and corresponding classes
+        self.__vals = []
+        self.__cs = []
+        self.class_names = ['yin', 'yang', 'dot']
+
+        # create data points with classes
+        for i in range(size):
+            # keep num of class instances balanced by using rejection sampling
+            # choose class for this sample
+            goal_class = self.rng.randint(3)
+            x, y, c = self.get_sample(goal=goal_class)
+
+            # add mirrod axis values
+            x_flipped = 1. - x
+            y_flipped = 1. - y
+
+            # append
+            self.__vals.append(np.array([x, y, x_flipped, y_flipped]))
+            self.__cs.append(c)
+
+    def get_sample(self, goal: Optional[int] = None) -> Tuple[float, ...]:
+        """
+        Sample one data point from the yin-yang sign with goal class `goal`. If
+        `goal` is None any sample of any class is returned.
+
+        :param goal: The target class of the sample to return. If None is
+            given, any sample regardless of its class is retuned.
+
+        :returns: Returns a tuple (x coordiante, y coordinate, class), where
+            the coordinates are on the xy-plane.
+        """
+        # sample until goal is satisfied
+        found_sample_yet = False
+        while not found_sample_yet:
+            # sample (x, y) coordinates
+            x, y = self.rng.rand(2) * 2. * self.r_big
+
+            # check if within yin-yang circle
+            if np.sqrt((x - self.r_big)**2 + (y - self.r_big)**2) > self.r_big:
+                continue
+
+            # check if they have the same class as the goal for this sample
+            c = self.which_class(x, y)
+
+            # check if class is accepted
+            if goal is None or c == goal:
+                found_sample_yet = True
+                break
+
+        return x, y, c
+
+    def which_class(self, x: float, y: float) -> int:
+        """
+        Assign a sample on the xy-plane with coordinates (x, y) to its class.
+
+        :param x: The x-coordinate.
+        :param y: The y-coordinate.
+
+        :returns: Am integer indicating the samples class.
+        """
+        # equations inspired by
+        # https://link.springer.com/content/pdf/10.1007/11564126_19.pdf
+        d_right = self.dist_to_right_dot(x, y)
+        d_left = self.dist_to_left_dot(x, y)
+        criterion1 = d_right <= self.r_small
+        criterion2 = d_left > self.r_small and d_left <= 0.5 * self.r_big
+        criterion3 = y > self.r_big and d_right > 0.5 * self.r_big
+
+        # check whether sample is in yin
+        is_yin = criterion1 or criterion2 or criterion3
+
+        # check whether sample is in eyes
+        is_circles = d_right < self.r_small or d_left < self.r_small
+
+        if is_circles:
+            return 2
+
+        return int(is_yin)
+
+    def dist_to_right_dot(self, x: int, y: int) -> float:
+        """
+        Compute the distance to the right dot.
+
+        :param x: The x-coordinate.
+        :param y: The y-coordinate.
+
+        :returns: Returns the distance to the right dot.
+        """
+        return np.sqrt((x - 1.5 * self.r_big)**2 + (y - self.r_big)**2)
+
+    def dist_to_left_dot(self, x: int, y: int) -> float:
+        """
+        Compute the distance to the left dot.
+
+        :param x: The x-coordinate.
+        :param y: The y-coordinate.
+
+        :returns: Returns the distance to the left dot.
+        """
+        return np.sqrt((x - 0.5 * self.r_big)**2 + (y - self.r_big)**2)
+
+    def __getitem__(self, index: int) -> Tuple[np.ndarray, int]:
+        """
+        Get an item from the dataset.
+
+        :param index: Index of the item in the dataset.
+
+        :returns: Returns a tuple (sample, target), where sample is an array
+            with values (x, y, 1 - x, 1 - y).
+        """
+        sample, target = (self.__vals[index].copy(), self.__cs[index])
+
+        # apply transformation
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, target
+
+    def __len__(self) -> int:
+        return len(self.__cs)
