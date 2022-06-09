c93870f5ec84974a206d7b8457695c575c49dcaa
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2022-06-09 10:14:46 +0200

    Adapt to c/17440
    Depends-On: 17440
    Change-Id: Iaa7756749338737b995e65cdcadb5c9fd66f0db9

--- src/hxtorch/snn/run.cpp
+++ src/hxtorch/snn/run.cpp
@@ -16,14 +16,8 @@ grenade::vx::IODataMap run(
 		throw std::runtime_error("No connection present.");
 	}
 
-	auto& executor = *hxtorch::detail::getConnection();
-	auto connection = executor.release_connection(halco::hicann_dls::vx::DLSGlobal());
-
-	auto ret = grenade::vx::network::run(connection, config, network_graph, inputs, playback_hooks);
-
-	executor.acquire_connection(halco::hicann_dls::vx::DLSGlobal(), std::move(connection));
-
-	return ret;
+	return grenade::vx::network::run(
+	    *detail::getConnection(), config, network_graph, inputs, playback_hooks);
 }
 
 }
