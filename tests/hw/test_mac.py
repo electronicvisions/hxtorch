9a6fbb7694d082885a90598ef6dc1a1ece54988f
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2022-06-24 11:19:59 +0200

    Add performance evaluation of MAC operation
    * includes sweep over batch size and quadratic matrix size
    * asserts performance for largest operation
    
    Change-Id: I06b86e00f0cfcb610fe0dcecaf21ef3cc6ee05ec

--- tests/hw/test_mac.py
+++ tests/hw/test_mac.py
@@ -2,8 +2,12 @@ from functools import partial
 import unittest
 import torch
 import hxtorch
+import time
 
+hxtorch.logger.reset()
 hxtorch.logger.default_config(level=hxtorch.logger.LogLevel.INFO)
+hxtorch.logger.set_loglevel(hxtorch.logger.get('hxcomm'), hxtorch.logger.LogLevel.WARN)
+hxtorch.logger.set_loglevel(hxtorch.logger.get('grenade'), hxtorch.logger.LogLevel.WARN)
 
 class TestHXMAC(unittest.TestCase):
     mac = hxtorch.mac
@@ -57,6 +61,67 @@ class TestHXMACmock(TestHXMAC):
     @classmethod
     def tearDownClass(cls):
         pass
+
+
+class TestHXMACPerformance(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        hxtorch.init_hardware()
+
+    @classmethod
+    def tearDownClass(cls):
+        hxtorch.release_hardware()
+
+    def test_sweep_batchsize(self):
+        ops = None
+        duration = None
+        for batch in range(14):
+            weights_in = (torch.rand(
+                hxtorch.constants.hardware_matrix_height * 2,
+                hxtorch.constants.hardware_matrix_width) - 0.5) \
+                * hxtorch.constants.synaptic_weight_max
+            data_in = torch.rand(
+                2**batch, hxtorch.constants.hardware_matrix_height * 2) \
+                * hxtorch.constants.input_activation_max
+            # perform unmeasured pre-run to warm caches
+            hxtorch.mac(data_in, weights_in)
+            # measured run
+            begin = time.time()
+            hxtorch.mac(data_in, weights_in)
+            duration = time.time() - begin
+            ops = weights_in.shape[0] * weights_in.shape[1] * data_in.shape[0]
+            hxtorch.logger.get(
+                f"{self.__class__.__name__}.test_sweep_batchsize").INFO(
+                "MAC performance {:.0f} op/s ".format(ops / duration) +
+                "at batchsize: {} and matrix size: {}.".format(
+                    data_in.shape[0], weights_in.shape))
+        # for largest batchsize expect 300Mop/s with 20% allowed deviation
+        self.assertGreater(ops / duration, 300e6 * 0.8)
+
+
+    def test_sweep_matrixsize(self):
+        ops = None
+        duration = None
+        for size in range(11):
+            weights_in = (torch.rand(2**size, 2**size) - 0.5) \
+                * hxtorch.constants.synaptic_weight_max
+            data_in = torch.rand(2000, 2**size) \
+                * hxtorch.constants.input_activation_max
+            # perform unmeasured pre-run to warm caches
+            hxtorch.mac(data_in, weights_in)
+            # measured run
+            begin = time.time()
+            hxtorch.mac(data_in, weights_in)
+            duration = time.time() - begin
+            ops = 2000 * 2**size * 2**size
+            hxtorch.logger.get(
+                f"{self.__class__.__name__}.test_sweep_matrixsize").INFO(
+                "MAC performance {:.0f} op/s ".format(ops / duration) +
+                "at batch size: {} and matrix size: {}.".format(
+                    2000, weights_in.shape))
+        # for largest matrixsize expect 330Mop/s with 20% allowed deviation
+        self.assertGreater(ops / duration, 330e6 * 0.8)
+
 
 if __name__ == '__main__':
     unittest.main()
