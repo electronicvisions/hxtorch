f16d7e274ef9ff9f29b9bd60dc289b826ab3e150
Elias Arnold <eliasarnold@outlook.de>
2023-06-18 15:41:51 +0200

    Fix pylint again
    Change-Id: I4dd2d23bc10c29fe755c64fabb6474c41c55436d

--- src/pyhxtorch/hxtorch/spiking/modules/types.py
+++ src/pyhxtorch/hxtorch/spiking/modules/types.py
@@ -5,15 +5,16 @@ import torch
 from hxtorch.spiking.modules.hx_module import HXModule
 
 
+# c.f.: https://github.com/pytorch/pytorch/issues/42305
+# pylint: disable=abstract-method
 class Population(HXModule):
     """ Base class for populations on BSS-2 """
-    # pylint: disable=abstract-method
-    pass
 
 
+# c.f.: https://github.com/pytorch/pytorch/issues/42305
+# pylint: disable=abstract-method
 class Projection(HXModule):
     """ Base class for projections on BSS-2 """
-    # pylint: disable=abstract-method
     __constants__ = ['in_features', 'out_features']
     in_features: int
     out_features: int
